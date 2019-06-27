041446433c6b53779c4baeddafa58c071bd0d642
ライ・ヴァン・ハイ <vlai@netprotections.co.jp>
2019-06-27 20:34:43 +0900

    Create API

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post "/pockers/check" => "pockers#check"
   get "/pockers/check" => "pockers#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount API::Base => '/api/'
 end
